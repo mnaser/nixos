b481eb8ac3e85043be3f149cace39eee3d66ed6d
Mohammed Naser <mnaser@vexxhost.com>
2025-09-20 23:00:11 -0400

    add teams

--- home.nix
+++ home.nix
@@ -16,6 +16,10 @@
   # changes in each release.
   home.stateVersion = "25.05";
 
+  home.packages = with pkgs; [
+    teams-for-linux
+  ];
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
