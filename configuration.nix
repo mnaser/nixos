cb7fdf510057464fb5b7aa714bb639ed8550b4fb
Mohammed Naser <mnaser@vexxhost.com>
2025-10-02 22:43:27 -0400

    add niri

--- configuration.nix
+++ configuration.nix
@@ -38,11 +38,14 @@
 
   # Enable the GNOME Desktop Environment.
   services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
-  services.desktopManager.gnome.extraGSettingsOverrides = ''
-    [org.gnome.mutter]
-    experimental-features=['scale-monitor-framebuffer', 'xwayland-native-scaling']
-  '';
+  #services.desktopManager.gnome.enable = true;
+  #services.desktopManager.gnome.extraGSettingsOverrides = ''
+  #  [org.gnome.mutter]
+  #  experimental-features=['scale-monitor-framebuffer', 'xwayland-native-scaling']
+  #'';
+
+  # Niri
+  programs.niri.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
--- flake.nix
+++ flake.nix
@@ -7,12 +7,15 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+    niri.url = "github:sodiboo/niri-flake";
+    niri.inputs.nixpkgs.follows = "nixpkgs";
+
     nixvim.url = "github:nix-community/nixvim";
     nixvim.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
-    { nixpkgs, home-manager, nixvim, ... }:
+    { nixpkgs, home-manager, niri, nixvim, ... }:
     {
       nixosConfigurations = {
         nixos = nixpkgs.lib.nixosSystem {
@@ -20,12 +23,16 @@
           modules = [
             ./configuration.nix
             home-manager.nixosModules.home-manager
+{
+  nixpkgs.overlays = [ niri.overlays.niri ];
+}
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.users.mnaser = ./home.nix;
 
               home-manager.sharedModules = [
+		niri.homeModules.niri
                 nixvim.homeModules.nixvim
               ];
 
