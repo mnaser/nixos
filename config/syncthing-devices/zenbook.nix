1e2dc956d746c313147bd76d0339f91a291372cd
Mohammed Naser <mnaser@vexxhost.com>
2025-10-11 11:13:37 -0400

    add zenbook syncthing device configuration
    Signed-off-by: Mohammed Naser <mnaser@vexxhost.com>

---
+++ config/syncthing-devices/zenbook.nix
@@ -0,0 +1,9 @@
+{ ... }:
+
+{
+  services.syncthing.settings.devices = {
+    zenbook = {
+      id = "TCJCGXO-QYXIZTD-S4GOTCL-AVVGOFK-ODSVGQD-DUQIZEL-WRP4TAR-TCERDAT";
+    };
+  };
+}
--- hosts/rig/configuration.nix
+++ hosts/rig/configuration.nix
@@ -14,6 +14,7 @@
     ../../config/nixos/tailscale.nix
     ../../config/nixos/evolution.nix
     ../../config/nixos/syncthing.nix
+    ../../config/syncthing-devices/zenbook.nix
   ];
 
   # Bootloader.
