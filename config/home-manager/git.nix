b2a81fd1f9d456979dfd55bd66ebace225619aef
Mohammed Naser <mnaser@vexxhost.com>
2025-10-11 12:11:33 -0400

    refactor: remove git signing configuration from home-manager and move it to 1password setup
    Signed-off-by: Mohammed Naser <mnaser@vexxhost.com>

--- config/home-manager/git.nix
+++ config/home-manager/git.nix
@@ -6,13 +6,6 @@ rec {
   programs.git.userName = "Mohammed Naser";
   programs.git.userEmail = "mnaser@vexxhost.com";
 
-  programs.git.signing = {
-    format = "ssh";
-    signer = lib.getExe' pkgs._1password-gui "op-ssh-sign";
-    signByDefault = true;
-    key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFUZ4e9irE5jBQZKxpqnoeJns6pmSUNNW0xVTJWB31zF";
-  };
-
   programs.git.hooks = {
     prepare-commit-msg = pkgs.writeShellScript "prepare-commit-msg.sh" ''
       ${lib.getExe' programs.git.package "git"} interpret-trailers --if-exists doNothing --trailer \
--- config/nixos/1password.nix
+++ config/nixos/1password.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, pkgs, ... }:
 
 {
   programs._1password.enable = true;
@@ -6,4 +6,15 @@
     enable = true;
     polkitPolicyOwners = [ "mnaser" ];
   };
+
+  home-manager.users.mnaser =
+    { ... }:
+    {
+      programs.git.signing = {
+        format = "ssh";
+        signer = lib.getExe' pkgs._1password-gui "op-ssh-sign";
+        signByDefault = true;
+        key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFUZ4e9irE5jBQZKxpqnoeJns6pmSUNNW0xVTJWB31zF";
+      };
+    };
 }
