0983cf6b573f46806fad55651ee961fb149a9e31
Mohammed Naser <mnaser@vexxhost.com>
2025-10-25 23:50:01 -0400

    new version cleanups
    Signed-off-by: Mohammed Naser <mnaser@vexxhost.com>

--- config/home-manager/git.nix
+++ config/home-manager/git.nix
@@ -6,17 +6,22 @@ rec {
   programs.git.userName = "Mohammed Naser";
   programs.git.userEmail = "mnaser@vexxhost.com";
 
+  programs.git.settings = {
+    user = {
+      name = "Mohammed Naser";
+      email = "mnaser@vexxhost.com";
+    };
+
+    init = {
+      defaultBranch = "main";
+    };
+  };
+
   programs.git.hooks = {
     prepare-commit-msg = pkgs.writeShellScript "prepare-commit-msg.sh" ''
       ${lib.getExe' programs.git.package "git"} interpret-trailers --if-exists doNothing --trailer \
           "Signed-off-by: ${programs.git.userName} <${programs.git.userEmail}>" \
           --in-place "$1"
     '';
-  };
-
-  programs.git.extraConfig = {
-    init = {
-      defaultBranch = "main";
-    };
   };
 }
